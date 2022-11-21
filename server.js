cf6263b34899edd91bc1482b5449e00d869912b3
Carlos Salgado <carlos.salgado@guros.com>
2022-11-21 14:22:01 -0600

    Feature: Update configuration in server file

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  res.send('Hello World Cuentame!');
 });
 
 app.listen(port, () => {
